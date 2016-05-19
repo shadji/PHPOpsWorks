33706b6de1bb421c07680c6164bc0f038d77958d
ap <ap@ubuntu-16.04-clean>
2016-05-19 11:06:13 +0300

    zmot_httpd

---
+++ zmot_httpd/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: zmot_httpd
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+cookbook_file "/etc/httpd/conf/httpd.conf" do
+  source "httpd.conf"
+  mode "0644"
+end
