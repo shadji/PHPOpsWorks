eaf4f29f044012f96ef870389deccca5bbb66dd6
Deyan Kalinov <Deyan Kalinov>
2016-05-22 11:44:37 +0300

    .

---
+++ nfs/metadata.rb
@@ -0,0 +1,15 @@
+maintainer 'Eric G. Wolfe'
+maintainer_email 'eric.wolfe@gmail.com'
+license 'Apache 2.0'
+description 'Installs and configures NFS, and NFS exports'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name 'nfs'
+version '2.2.8'
+source_url 'https://github.com/atomic-penguin/cookbook-nfs' if respond_to?(:source_url)
+issues_url 'https://github.com/atomic-penguin/cookbook-nfs/issues' if respond_to?(:issues_url)
+
+%w(ubuntu debian redhat centos fedora scientific amazon oracle sles freebsd).each do |os|
+  supports os
+end
+
+depends 'line'
