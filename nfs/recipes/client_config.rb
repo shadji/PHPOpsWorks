0fe2d1197b9fcbd6496470f435e9ccb32b2f92c1
Deyan Kalinov <Deyan Kalinov>
2016-05-22 14:11:34 +0300

    Client config

--- nfs/recipes/client_config.rb
+++ nfs/recipes/client_config.rb
@@ -8,7 +8,7 @@ directory node['nfs_client']['nfs_mount_point'] do
 end
 
 mount node['nfs_client']['nfs_mount_point'] do
-  device node['nfs_client']['nfs_mount_point'] + ":" + node['nfs']['export']
+  device node['nfs_client']['nfs_server'] + ":" + node['nfs']['export']
   fstype 'nfs'
   options 'rw'
   action [:mount, :enable]
