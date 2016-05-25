746f52b6b5bc4f73b9fa1478cafe8f369a282743
Alexander Panov <a.panov@gmail.com>
2016-05-25 18:14:59 +0300

    perlbrew

--- perlbrew/attributes/default.rb
+++ perlbrew/attributes/default.rb
@@ -25,7 +25,7 @@ default['perlbrew']['perls'] = []
 # default command line options for "perlbrew install" if no LWRP options
 # attribute is given
 default['perlbrew']['install_options'] = ''
-default['perlbrew']['cpanm_options'] = ''
+default['perlbrew']['cpanm_options'] = '--notest'
 default['perlbrew']['self_upgrade'] = true
 
 default['perlbrew']['profile']['group'] = 'root'
--- perlbrew/recipes/default.rb
+++ perlbrew/recipes/default.rb
@@ -27,3 +27,11 @@ perlbrew '/opt/perlbrew' do
     perls         [ 'perl-5.8.8' ]
     upgrade       true
   end
+
+perlbrew_switch 'perl-5.8.8'
+
+perlbrew_cpanm 'Modern Perl modules' do
+    modules ['DBI', 'Text::CSV', 'JSON', 'Canary::Stability', 'JSON::XS', 'LWP', 'Module::Build', 'Mail::IMAPClient', 'XML::LibXML', 'Authen::SASL', 'Net::FTP', 'Net::FTPSSL', 'Web::Scraper', 'Mail::POP3Client', 'MIME::Parser', 'DateTime', 'DateTime::Format::MySQL', 'Math::GMP', 'Net::SSH::Perl', 'Math::Pari', 'Net::SFTP']
+    perlbrew 'perl-5.8.8@mylib'
+ end
+
