338d0d93dc3258a55c80b092f6435bcdf86e9ca3
Slav Hadzhidimitrov <slavhadji@gmail.com>
2016-05-27 01:56:05 -0700

    skip PHP apps on script layers

--- scripts/recipes/sync.rb
+++ scripts/recipes/sync.rb
@@ -2,6 +2,11 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
 
+  if deploy[:application_type] == 'php'
+    Chef::Log.debug("Skipping PHP apps")
+    next
+  end
+
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
