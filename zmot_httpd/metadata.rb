4071ac920a50197ab47850f1a04abed0e7f4831c
Slav Hadzhidimitrov <slavhadji@gmail.com>
2016-05-27 01:26:34 -0700

    cleanup

--- zmot_httpd/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'zmot_httpd'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'a.panov@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures zmot_httpd'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
