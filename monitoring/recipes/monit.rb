33a0c997e6785895027059fb60216905eb4b2f14
Deyan Kalinov <Deyan Kalinov>
2016-05-28 15:53:23 +0300

    Monitoring recepies

--- monitoring/recipes/monit.rb
+++ monitoring/recipes/monit.rb
@@ -4,12 +4,12 @@ template "/etc/monit.conf" do
   mode 0600
   source 'monit.erb'
   variables(
-	  :send_alert => "#{node[:alerts][:send_alert]}",
+	  :send_alert => node[:alerts][:send_alert],
 	  :smtp => "#{node[:alerts][:smtp]}",
 	  :port => "#{node[:alerts][:port]}",
 	  :username => "#{node[:alerts][:username]}",
 	  :password => "#{node[:alerts][:password]}",
-	  :useTLS => "#{node[:alerts][:useTLS]}",
+	  :useTLS => node[:alerts][:useTLS],
 	  :emails => node[:alerts][:emails]
 	)
   notifies :restart, "service[monit]" 
