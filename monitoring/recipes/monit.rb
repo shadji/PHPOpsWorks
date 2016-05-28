fa51f1cb5bef888dcdb7d9edba3a36652b497499
Deyan Kalinov <Deyan Kalinov>
2016-05-28 14:47:07 +0300

    Monitoring recepies

---
+++ monitoring/recipes/monit.rb
@@ -0,0 +1,20 @@
+template "/etc/monit.conf" do
+  owner 'root'
+  group 'root'
+  mode 0600
+  source 'monit.erb'
+  variables(
+	  :send_alert => node['alerts']['send_alert'],
+	  :smtp => node['alerts']['smtp'],
+	  :port => node['alerts']['port'],
+	  :username => node['alerts']['username'],
+	  :password => node['alerts']['password'],
+	  :useTLS => node['alerts']['useTLS'],
+	  :emails => node['alerts']['emails']
+	)
+  notifies :restart, "service[monit]" 
+end
+
+service 'monit' do
+  action :start
+end
