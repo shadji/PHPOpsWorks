3d84f24bc6b7fd2e18f497f5e817bb5f6c1ca26a
Deyan Kalinov <Deyan Kalinov>
2016-05-28 18:36:13 +0300

    Monitoring recepies

--- monitoring/recipes/monit.rb
+++ monitoring/recipes/monit.rb
@@ -1,3 +1,6 @@
+Chef::Log.info("Updateing monit ...")
+Chef::Log.info(`sudo yum install -y https://kojipkgs.fedoraproject.org//packages/monit/5.14/1.el6/x86_64/monit-5.14-1.el6.x86_64.rpm`)
+
 template "/etc/monit.conf" do
   owner 'root'
   group 'root'
