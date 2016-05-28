7ec2f4e5433525a36e5db0a2b9212e7ccf292123
Deyan Kalinov <Deyan Kalinov>
2016-05-28 21:35:30 +0300

    Deploy hook removed

--- deploy_scripts/recipes/create_links_to_nfs.rb
+++ deploy_scripts/recipes/create_links_to_nfs.rb
@@ -10,7 +10,7 @@ node[:deploy].each do |application, deploy|
 		Chef::Log.info(`sudo chown #{user}:#{group} #{site.link}`)
 		Chef::Log.info(`sudo chmod #{mode} #{site.link}`)
 		Chef::Log.info(`sudo chmod g+s #{site.link}`)
-		Chef::Log.info("Createing symlink: sudo -H -u #{user} bash -c 'cd #{deploy[:deploy_to]} && ln -s #{site.link} ./#{site.name}/#{site.directory}")
+		Chef::Log.info("Createing symlink: sudo -H -u #{user} bash -c 'cd #{deploy[:deploy_to]}/current && ln -s #{site.link} ./#{site.name}/#{site.directory}")
 		Chef::Log.info(`sudo -H -u #{user} bash -c 'cd #{deploy[:deploy_to]} && ln -s #{site.link} ./#{site.name}/#{site.directory}'`)
 		raise "Symlink failed, server wonn't come up" unless $?.success?
 	end
