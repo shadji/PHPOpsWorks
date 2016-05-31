32d323504015d6836772e1ac8bc767e632c2b5bb
Slav Hadzhidimitrov <slavhadji@gmail.com>
2016-05-31 18:32:13 +0300

    PHP ini override 2

--- deploy_scripts/recipes/php_ini.rb
+++ deploy_scripts/recipes/php_ini.rb
@@ -1,4 +1,4 @@
 include_recipe 'php'
 
-node.default['php']['install_method'] = ''
+node.default['php']['packages'] = %w(php53 php53-devel php53-cli php-pear)
 node.default['php']['date.timezone'] = 'America/Los_Angeles'
