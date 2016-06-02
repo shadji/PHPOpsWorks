8fcbcd625cf2f0b159df91bc3309c03e65644c48
Slav Hadjidimitrov <shadji@users.noreply.github.com>
2016-06-02 09:16:09 +0300

    Create setenv.rb

---
+++ deploy_scripts/recipes/setenv.rb
@@ -0,0 +1,11 @@
+  template ::File.join(params[:path], "app.env") do
+    source "app.env.erb"
+    cookbook "opsworks_nodejs"
+    mode 0770
+    owner params[:user]
+    group params[:group]
+    variables(
+      :environment => OpsWorks::Escape.escape_double_quotes(params[:environment_variables])
+    )
+    only_if { ::File.exists?(params[:path]) }
+  end
