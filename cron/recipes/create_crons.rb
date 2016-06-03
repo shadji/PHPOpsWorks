45fc244a712944ca75c9a49796f961ac4dfac3a2
Alexander Panov <a.panov@gmail.com>
2016-06-03 13:30:03 +0300

    cron change the default mailer

--- cron/recipes/create_crons.rb
+++ cron/recipes/create_crons.rb
@@ -1,11 +1,18 @@
 template "/home/ec2-user/bash_wrapper.sh" do
   owner 'ec2-user'
-  group 'root'
+  group 'ec2-user'
   mode 0774
   source 'bash_wrapper.erb'  
 end
-my_env_vars = {"TZ" => "America/New_York"}
 
+template "/etc/sysconfig/crond" do
+  owner 'root'
+  group 'root'
+  mode 0600
+  source 'crond.erb'
+end
+
+my_env_vars = {"TZ" => "America/New_York"}
 cron 'test.pl' do
         user'ec2-user'
         environment my_env_vars
