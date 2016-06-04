64f59df3bfa7270fee5d5534b61f3140eaa837db
Deyan Kalinov <Deyan Kalinov>
2016-06-04 17:14:25 +0300

    Create group

--- deploy_scripts/recipes/create_nfs_group.rb
+++ deploy_scripts/recipes/create_nfs_group.rb
@@ -1,4 +1,18 @@
+group 'apache' do  
+  gid 48
+end
+
+user 'apache' do
+  comment 'Apache'
+  uid '48'
+  gid '48'
+  home '/var/www'
+  shell '/sbin/nologin'
+end
+
 group 'nfs-share' do  
   members ['ec2-user', 'apache', 'nobody']
   gid 5001
 end
+
+	
