7ed86638d98921b87a48ded349d16cad7499e43b
Slav Hadzhidimitrov <slavhadji@gmail.com>
2016-06-04 14:25:20 +0300

    sync s3 recipe

--- deploy_scripts/recipes/nfs_sync_cron.rb
+++ deploy_scripts/recipes/nfs_sync_cron.rb
@@ -1,7 +1,7 @@
 ruby_block "set-up S3 Archive cron on NFS storage service" do
     block do
         Chef::Log.info("Creating cron job for S3 Sync")
-        env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['admin_scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['admin_scripts'][:environment_variables][:REGION]}"]
+        env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:REGION]}"]
 
         cron 'S3_sync' do
             user'ec2-user'
