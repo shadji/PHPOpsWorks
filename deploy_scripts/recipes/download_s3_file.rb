639f628c4cef1d29fe4c93879ea0ff5dedc31247
Deyan Kalinov <Deyan Kalinov>
2016-06-05 21:06:32 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -5,6 +5,6 @@ include_recipe 's3_file'
 s3_file "/srv/www/t2" do
   bucket "mybucket.deyan"
   remote_path "t2"
-  aws_access_key_id lazy {'#{node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}'}
-  aws_secret_access_key lazy {'#{node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}'}
+  aws_access_key_id lazy {"#{node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}"}
+  aws_secret_access_key lazy {"#{node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}"}
 end
