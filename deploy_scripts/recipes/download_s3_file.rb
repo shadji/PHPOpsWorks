1c4c6fdaf353d4142fc111cbe4a8705ea25c886b
Deyan Kalinov <Deyan Kalinov>
2016-06-05 20:52:22 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -13,10 +13,10 @@ include_recipe 's3_file'
 
 
 Chef::Log.info("'#{ node[:AWS_ACCESS_KEY_ID]}")
-Chef::Log.info("'#{ node[:AWS_ACCESS_KEY_ID]}'")
+Chef::Log.info("'#{ node[:AWS_SECRET_ACCESS_KEY]}'")
 s3_file "/srv/www/t2" do
   bucket "mybucket.deyan"
   remote_path "t2"
   aws_access_key_id node[:AWS_ACCESS_KEY_ID]
-  aws_secret_access_key node[:AWS_ACCESS_KEY_ID]
+  aws_secret_access_key node[:AWS_SECRET_ACCESS_KEY]
 end
