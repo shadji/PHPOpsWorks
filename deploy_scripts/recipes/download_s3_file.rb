ab1ed090ffe3435eb246dc3f62deaf9f03b4b397
Deyan Kalinov <Deyan Kalinov>
2016-06-05 19:49:45 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -1,6 +1,3 @@
-
-$AWS_ACCESS_KEY_ID = ''
-$AWS_SECRET_ACCESS_KEY = ''
 ruby_block "Get file from s3" do
     block do
 		
@@ -10,23 +7,12 @@ ruby_block "Get file from s3" do
 	end
 end
 
-ruby_block "Get file from s3" do
-    block do
-		
-		Chef::Log.info("'#{ $AWS_ACCESS_KEY_ID}")
-		Chef::Log.info("'#{ $AWS_SECRET_ACCESS_KEY}'")
-	
-	end
-end
-
-
-
-
 include_recipe 's3_file'
 
-
+Chef::Log.info("'#{ $AWS_ACCESS_KEY_ID}")
+Chef::Log.info("'#{ $AWS_SECRET_ACCESS_KEY}'")
 s3_file "/srv/www/t2" do
-bucket "mybucket.deyan"
+  bucket "mybucket.deyan"
   remote_path "t2"
   aws_access_key_id $AWS_ACCESS_KEY_ID
   aws_secret_access_key $AWS_SECRET_ACCESS_KEY
