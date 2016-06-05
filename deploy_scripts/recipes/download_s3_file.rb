f5556b6b37599aa6e6d26651779fa5eb062b7780
Deyan Kalinov <Deyan Kalinov>
2016-06-05 20:58:18 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -1,22 +1,10 @@
 
-ruby_block "Get file from s3" do
-    block do
-		
-		node.default["AWS_ACCESS_KEY_ID"] =  node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
-		node.default["AWS_SECRET_ACCESS_KEY"] =  node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
-	
-	end
-end
-
 
 include_recipe 's3_file'
 
-
-Chef::Log.info("'#{ node[:AWS_ACCESS_KEY_ID]}")
-Chef::Log.info("'#{ node[:AWS_SECRET_ACCESS_KEY]}'")
 s3_file "/srv/www/t2" do
   bucket "mybucket.deyan"
   remote_path "t2"
-  aws_access_key_id node[:AWS_ACCESS_KEY_ID]
-  aws_secret_access_key node[:AWS_SECRET_ACCESS_KEY]
+  aws_access_key_id lazy {'#{node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}'}
+  aws_secret_access_key lazy {'#{node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}'}
 end
