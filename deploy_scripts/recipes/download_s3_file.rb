e8b16e9ee205c30f4f325723818f4ce9e6b5e2a0
Deyan Kalinov <Deyan Kalinov>
2016-06-05 18:19:49 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -6,8 +6,8 @@ ruby_block "Get file from s3" do
 		aws_s3_file "/srv/www/t2" do
 		  bucket "mybucket.deyan"
 		  remote_path "t2"
-		  aws_access_key_id node[:deploy]['admin_scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
-		  aws_secret_access_key node[:deploy]['admin_scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
+		  aws_access_key_id node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
+		  aws_secret_access_key node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
 		end
 	end
 end
