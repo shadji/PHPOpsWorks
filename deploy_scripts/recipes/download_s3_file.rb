4c5c4bce1fe45247d94bf429d7dc85ab52f924cc
Deyan Kalinov <Deyan Kalinov>
2016-06-05 18:33:29 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -1,19 +1,23 @@
-include_recipe 'aws'
-@AWS_ACCESS_KEY_ID = ''
-@AWS_SECRET_ACCESS_KEY = ''
+chef_gem 'aws-sdk' do
+  compile_time false
+  action :install
+end
+
+$AWS_ACCESS_KEY_ID = ''
+$AWS_SECRET_ACCESS_KEY = ''
 ruby_block "Get file from s3" do
     block do
 		
-		@AWS_ACCESS_KEY_ID =  node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
-		@AWS_SECRET_ACCESS_KEY =  node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
+		$AWS_ACCESS_KEY_ID =  node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
+		$AWS_SECRET_ACCESS_KEY =  node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
 	
 	end
 end
 
-
+include_recipe 'aws'
 aws_s3_file "/srv/www/t2" do
   bucket "mybucket.deyan"
   remote_path "t2"
-  aws_access_key_id @AWS_ACCESS_KEY_ID
-  aws_secret_access_key @AWS_SECRET_ACCESS_KEY
+  aws_access_key_id $AWS_ACCESS_KEY_ID
+  aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 end
