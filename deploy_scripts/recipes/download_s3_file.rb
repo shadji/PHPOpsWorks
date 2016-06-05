5033fb7bdbff5dde36f4886131ceaea70557a5a9
Deyan Kalinov <Deyan Kalinov>
2016-06-05 20:23:01 +0300

    S3 script

--- deploy_scripts/recipes/download_s3_file.rb
+++ deploy_scripts/recipes/download_s3_file.rb
@@ -1,13 +1,24 @@
+node[:AWS_ACCESS_KEY_ID][:content] = ''
+node[:AWS_SECRET_ACCESS_KEY][:content] = ''
+
 ruby_block "Get file from s3" do
     block do
-		run_context.include_recipe 's3_file'
-		s3_file "/srv/www/t2" do
-		  bucket "mybucket.deyan"
-		  remote_path "t2"
-		  aws_access_key_id node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
-		  aws_secret_access_key node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
-		  region us-east-1
-		end
+		
+		node[:AWS_ACCESS_KEY_ID][:content] =  node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]
+		node[:AWS_ACCESS_KEY_ID][:content] =  node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]
+	
 	end
 end
 
+
+include_recipe 's3_file'
+
+
+Chef::Log.info("'#{ node[:AWS_ACCESS_KEY_ID][:content]}")
+Chef::Log.info("'#{ node[:AWS_ACCESS_KEY_ID][:content]}'")
+s3_file "/srv/www/t2" do
+  bucket "mybucket.deyan"
+  remote_path "t2"
+  aws_access_key_id node[:AWS_ACCESS_KEY_ID][:content]
+  aws_secret_access_key node[:AWS_ACCESS_KEY_ID][:content]
+end
