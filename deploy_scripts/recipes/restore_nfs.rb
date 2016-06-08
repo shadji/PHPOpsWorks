9c1529488bef84b2a1fe1761a251c0492bc04cee
Deyan Kalinov <Deyan Kalinov>
2016-06-08 20:52:40 +0300

    NFS restore

--- deploy_scripts/recipes/restore_nfs.rb
+++ deploy_scripts/recipes/restore_nfs.rb
@@ -8,7 +8,7 @@ ruby_block 'restore' do
 		aws_access_key_id = lazy {"#{node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}"}
 		aws_secret_access_key = lazy {"#{node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}"}
 		Chef::Log.info("export AWS_ACCESS_KEY_ID=#{aws_access_key_id}; export AWS_SECRET_ACCESS_KEY=#{aws_secret_access_key}; aws s3 sync s3://#{node['s3']['bucket']} #{node['nfs']['export']}")
-        Chef::Log.info(`export AWS_ACCESS_KEY_ID=#{aws_access_key_id}; export AWS_SECRET_ACCESS_KEY=#{aws_secret_access_key}; aws s3 sync s3://#{node['s3']['bucket']} #{node['nfs']['export']}`)
+        Chef::Log.info(`export AWS_ACCESS_KEY_ID=#{aws_access_key_id} && export AWS_SECRET_ACCESS_KEY=#{aws_secret_access_key} && aws s3 sync s3://#{node['s3']['bucket']} #{node['nfs']['export']}`)
         raise "Restore failed" unless $?.success?
 	end
 end
